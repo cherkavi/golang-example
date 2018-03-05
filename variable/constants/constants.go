2f4ad33a0c79b98b9a2f69acccf974f277cdde13
cherkavi <technik7job@gmail.com>
2018-03-05 21:38:05 +0100

    initialization of variables

---
+++ variable/constants/constants.go
@@ -0,0 +1,5 @@
+package constants
+
+var A int = 10
+var B string = "value from another package"
+var k string = "not visible variable"
---
+++ variable/variables.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"./constants"
+)
+
+func main() {
+	// declare variable with default value
+	var a int
+	fmt.Printf("default value for integer is %v \n", a)
+
+	// declare variable and init it
+	var b int = 2
+	fmt.Printf("declare and init value  %v\n", b)
+	// multiply assign
+	var x, y int = 10, 15
+	fmt.Println("multiply assignment: ", x, y)
+
+	// declare variable and not to mention about type ( automatic type assign )
+	c := 2
+	// multiply return value from function
+	bytesNumber, error := fmt.Printf("just init value: %v and assign type automatically: %T\n", c, c)
+
+	// multiply assignment
+	w, z := 10, 15
+	fmt.Println("multiply assignment with automatic type: ", w, z)
+
+	if error != nil {
+		fmt.Printf(" %v amount of bytes were written, with error %v", bytesNumber, error)
+	}
+
+	fmt.Println("value from another package: ", constants.A)
+
+}
