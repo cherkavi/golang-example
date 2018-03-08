15cfb8520456244a3a8cdc0118dc3750a1cce4cf
cherkavi <technik7job@gmail.com>
2018-03-08 21:13:06 +0100

    struct embedded and complex

--- struct/struct.go
+++ struct/struct.go
@@ -4,49 +4,61 @@ import (
 	. "fmt"
 )
 
-type Pair struct {
+type Point struct {
 	Left int
 	Right int
 }
 
 // new function of struct, read-only access
-func (p Pair) sum() int {
+func (p Point) sum() int {
 	return p.Left+p.Right
 }
 
 // new function of struct, read-write access
-func (p *Pair) increase(value int) {
+func (p *Point) increase(value int) {
 	p.Right += value
 	p.Left += value
 }
 
 // changes will not be applied
-func (p Pair) increaseNotAffected(value int) {
+func (p Point) increaseNotAffected(value int) {
 	p.Right += value
 	p.Left += value
 }
 
-func sum(p Pair) int{
+func sum(p Point) int{
 	return p.Left + p.Right
 }
 
-func increase (p *Pair, biase int){
+func increase (p *Point, biase int){
 	p.Right+=biase
 	p.Left+=biase
 }
 
+// complex type
+type Rectangle struct{
+	P1 Point
+	P2 Point
+}
+
+// embedded type
+type Pixel struct{
+	Point
+	Color string
+}
+
 func main() {
 	// declaration
-	Println(Pair{1, 2})
-	Println(Pair{Right: 2})
-	Println(Pair{})
+	Println(Point{1, 2})
+	Println(Point{Right: 2})
+	Println(Point{})
 
 	// slice declaration
-	Println("slice: ", []Pair{
+	Println("slice: ", []Point{
 			{2,3},{3,4},{5,6}, {7,8},
 		})
 
-	p := Pair{1,2}
+	p := Point{1,2}
 	Println("value:", p)
 	Println("sum:", p.sum(), "  ", sum(p))
 
@@ -61,4 +73,22 @@ func main() {
 
 	noNameStruct := struct{i int; s string} {i:10, s: "error_message"}
 	Println("anonymous struct: ",  noNameStruct )
+
+	anonymousSlice:= []struct{a int; b int} {
+		{1,2},
+		{3,4},
+		{4,5},
+		{5,6},
+	}
+	Println("slice of anonymous struct", anonymousSlice)
+
+	// complex value
+	r := Rectangle{P1:Point{1,2}, P2:Point{3,4}}
+	Println("rectangle: ", r)
+
+	pixel1 := Pixel{}
+	pixel1.Left = 3
+	pixel1.Right = 5
+	pixel1.Color = "red"
+	Println("Embedded struct: ", pixel1)
 }
