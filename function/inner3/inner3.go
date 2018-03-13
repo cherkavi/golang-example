f70456cbb31b6f41012046dc12b0d1cd7844fc01
cherkavi <technik7job@gmail.com>
2018-03-13 21:11:16 +0100

    fibonacci implementation

--- function/inner3/inner3.go
+++ function/inner3/inner3.go
@@ -2,6 +2,23 @@ package inner3
 
 import "fmt"
 
+func Fibonacci() func() int {
+	var beforePrevious, previous int = -1, 0
+	return func() int{
+		if beforePrevious==(-1){
+			beforePrevious=0
+			return 0
+		}
+		if previous==(0){
+			previous=1
+			return 1
+		}
+		var returnValue int = beforePrevious+previous
+		beforePrevious, previous = previous, returnValue
+		return returnValue
+	}
+}
+
 func init() {
 	fmt.Println("func init from package 'inner3' ")
 }
