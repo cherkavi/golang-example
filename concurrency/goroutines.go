ec414804f4c3af0a0466b58e2d766ac15bd3211b
cherkavi <technik7job@gmail.com>
2018-03-16 20:52:22 +0100

    rules for channel

--- concurrency/goroutines.go
+++ concurrency/goroutines.go
@@ -8,6 +8,12 @@ import (
 
 
 func main(){
+	//A send to a nil channel blocks forever
+	//A receive from a nil channel blocks forever
+	//A send to a closed channel panics
+	//A receive from a closed channel returns default init value immediately
+
+
 	fmt.Println("--- parallel threads execution ---")
 	simple.ExampleOfSimpleExecution()
 
