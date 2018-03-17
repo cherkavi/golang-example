353d641a40c20a17085883df92c9b708cfcb4ceb
cherkavi <technik7job@gmail.com>
2018-03-17 17:08:04 +0100

    embedded struct, nested struct

--- struct/struct.go
+++ struct/struct.go
@@ -5,13 +5,13 @@ import (
 )
 
 type Point struct {
-	Left int
+	Left  int
 	Right int
 }
 
 // new function of struct, read-only access
 func (p Point) sum() int {
-	return p.Left+p.Right
+	return p.Left + p.Right
 }
 
 // new function of struct, read-write access
@@ -26,24 +26,27 @@ func (p Point) increaseNotAffected(value int) {
 	p.Left += value
 }
 
-func sum(p Point) int{
+func sum(p Point) int {
 	return p.Left + p.Right
 }
 
-func increase (p *Point, biase int){
-	p.Right+=biase
-	p.Left+=biase
+func increase(p *Point, biase int) {
+	p.Right += biase
+	p.Left += biase
 }
 
 // complex type
-type Rectangle struct{
-	P1 Point
-	P2 Point
+type Rectangle struct {
+	P1 Point    // regular struct
+	P2 struct { // nested struct
+		Left  int
+		Right int
+	}
 }
 
 // embedded type
-type Pixel struct{
-	Point
+type Pixel struct {
+	Point // embedded struct
 	Color string
 }
 
@@ -55,10 +58,10 @@ func main() {
 
 	// slice declaration
 	Println("slice: ", []Point{
-			{2,3},{3,4},{5,6}, {7,8},
-		})
+		{2, 3}, {3, 4}, {5, 6}, {7, 8},
+	})
 
-	p := Point{1,2}
+	p := Point{1, 2}
 	Println("value:", p)
 	Println("sum:", p.sum(), "  ", sum(p))
 
@@ -71,20 +74,26 @@ func main() {
 	increase(&p, 7)
 	Println("increase via function:", p)
 
-	noNameStruct := struct{i int; s string} {i:10, s: "error_message"}
-	Println("anonymous struct: ",  noNameStruct )
+	noNameStruct := struct {
+		i int
+		s string
+	}{i: 10, s: "error_message"}
+	Println("anonymous struct: ", noNameStruct)
 
-	anonymousSlice:= []struct{a int; b int} {
-		{1,2},
-		{3,4},
-		{4,5},
-		{5,6},
+	anonymousSlice := []struct {
+		a int
+		b int
+	}{
+		{1, 2},
+		{3, 4},
+		{4, 5},
+		{5, 6},
 	}
 	Println("slice of anonymous struct", anonymousSlice)
 
 	// complex value
-	r := Rectangle{P1:Point{1,2}, P2:Point{3,4}}
-	Println("rectangle: ", r)
+	r := Rectangle{P1: Point{1, 2}, P2: Point{3, 4}}
+	Printf("rectangle: %#v \n", r)
 
 	pixel1 := Pixel{}
 	pixel1.Left = 3
@@ -92,8 +101,11 @@ func main() {
 	pixel1.Color = "red"
 	Println("Embedded struct: ", pixel1)
 
-	p1:=Point{1,2}
-	p2:=Point{1,2}
-	p3:=struct{Left int; Right int}{1,2}
-	Printf("point#1:%p %#v  point#2:%p %#v  point#3:%p %#v   point#1==point#2:%v   point#1==point#3:%v", &p1, p1, &p2, p2, &p3, p3, p1==p2, p1==p3, )
+	p1 := Point{1, 2}
+	p2 := Point{1, 2}
+	p3 := struct {
+		Left  int
+		Right int
+	}{1, 2}
+	Printf("point#1:%p %#v  point#2:%p %#v  point#3:%p %#v   point#1==point#2:%v   point#1==point#3:%v", &p1, p1, &p2, p2, &p3, p3, p1 == p2, p1 == p3)
 }
