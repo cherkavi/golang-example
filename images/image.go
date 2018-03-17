363a6a5e32e648b79eff60e134f8bb9c3c98e350
cherkavi <technik7job@gmail.com>
2018-03-17 14:29:15 +0100

    tempfile canvas image draw

---
+++ images/image.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"io/ioutil"
+)
+
+func main() {
+	fmt.Println("------------ image creation ")
+	width, height := 100, 100
+	canvas := image.NewRGBA(image.Rect(0, 0, width, height))
+	for i := width; i >= 0; i-- {
+		canvas.Set(i, height/2, color.RGBA{255, 0, 0, 255})
+	}
+
+	// fmt.Println(m.Bounds())
+	// fmt.Println(m.At(0, 0).RGBA())
+	tempFile, err := ioutil.TempFile("", "jpg")
+	if err != nil {
+		panic(err)
+	}
+	png.Encode(tempFile, canvas)
+	fmt.Println(tempFile.Name())
+	tempFile.Close()
+}
