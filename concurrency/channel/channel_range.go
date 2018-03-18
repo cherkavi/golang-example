0fae3d7a5f6486199157d449714cb593e3179f5b
cherkavi <technik7job@gmail.com>
2018-03-18 16:30:20 +0100

    readonly channel, writeonly channel

--- concurrency/channel/channel_range.go
+++ concurrency/channel/channel_range.go
@@ -5,28 +5,28 @@ import (
 	"time"
 )
 
-// declare writeonly channel
+// using usual channel as writeonly channel
 func writeIn(destination chan<- string) {
-	for i:=0;i<10;i++{
-		data:=fmt.Sprintf("%v", i)
+	for i := 0; i < 10; i++ {
+		data := fmt.Sprintf("%v", i)
 		fmt.Printf(" w->%v- ", data)
-		destination<-data
+		destination <- data
 	}
 	close(destination)
 }
 
-// declare readonly channel
-func readFrom(source <-chan string ){
-	for msg:= range source{
+// using usual channel as readonly channel
+func readFrom(source <-chan string) {
+	for msg := range source {
 		fmt.Printf(" %v->r ", msg)
 	}
 }
 
-func RangeAllFromChannel(){
+func RangeAllFromChannel() {
 	channel := make(chan string, 3)
 	go writeIn(channel)
 	go readFrom(channel)
-	time.Sleep(time.Second*1)
+	time.Sleep(time.Second * 1)
 	fmt.Println()
 	fmt.Println()
 }
