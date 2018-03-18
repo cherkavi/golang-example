880f8aee7888ca036b8894e1ed72d3a31e0cf182
cherkavi <technik7job@gmail.com>
2018-03-18 17:11:50 +0100

    goroutine parameter, goroutine closure

--- concurrency/goroutines.go
+++ concurrency/goroutines.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"fmt"
-	"./simple"
+
 	"./channel"
+	"./simple"
 )
 
-
-func main(){
+func main() {
 	//A send to a nil channel blocks forever
 	//A receive from a nil channel blocks forever
 	//A send to a closed channel panics
 	//A receive from a closed channel returns default init value immediately
-
 
 	fmt.Println("--- parallel threads execution ---")
 	simple.ExampleOfSimpleExecution()
@@ -28,4 +27,7 @@ func main(){
 
 	fmt.Println("--- using Mutex (Lock) ---")
 	channel.MutexUsing()
+
+	fmt.Println("--- goroutines with parameters ---")
+	simple.ExecuteWithParameter()
 }
---
+++ concurrency/simple/parameters.go
@@ -0,0 +1,37 @@
+package simple
+
+import (
+	"fmt"
+	"time"
+)
+
+func delayPrint(message string) {
+	time.Sleep(time.Second * 1)
+	fmt.Println("message from goroutine: " + message)
+}
+
+func delayExecution(externalFunction func()) {
+	time.Sleep(time.Second * 1)
+	externalFunction()
+}
+
+func ExecuteWithParameter() {
+	fmt.Printf("\n ---- attempt to print message after change it ---- \n")
+	message := "first message"
+	fmt.Printf("message before start goroutine: %v \n", message)
+	go delayPrint(message)
+	message = "changed message"
+	fmt.Printf("message after start goroutine: %v \n", message)
+	time.Sleep(time.Second * 2)
+
+	fmt.Printf("\n ---- attempt to execute function into closure after change environment ---- \n")
+	message = "first message"
+	fmt.Printf("message before start goroutine: %v \n", message)
+	go delayExecution(func() {
+		fmt.Println("message from goroutine: " + message)
+	})
+	message = "changed message"
+	fmt.Printf("message after start goroutine: %v \n", message)
+	time.Sleep(time.Second * 2)
+
+}
