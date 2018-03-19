06220fff2f28d781312f72665c1648506f34e787
cherkavi <technik7job@gmail.com>
2018-03-19 21:07:45 +0100

    multi init method

--- init/init.go
+++ init/init.go
@@ -10,6 +10,14 @@ func init() {
 	fmt.Println("init method of main package ")
 }
 
+func init() {
+	fmt.Println("init method ( second ) of main package ")
+}
+
+func init() {
+	fmt.Println("init method ( third ) of main package ")
+}
+
 func main() {
 	fmt.Println("main method of main package was started ")
 	subpackage.JustEmptyFunction()
--- init/subpackage/init2.go
+++ init/subpackage/init2.go
@@ -8,5 +8,13 @@ func init() {
 	fmt.Println("  init method into package subinit")
 }
 
+func init() {
+	fmt.Println("  init method (second) into package subinit")
+}
+
+func init() {
+	fmt.Println("  init method (third) into package subinit")
+}
+
 func JustEmptyFunction() {
 }
