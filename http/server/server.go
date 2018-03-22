9813b8ef7d10b992b17e02844c3baf1cfe4351a0
cherkavi <technik7job@gmail.com>
2018-03-22 18:27:54 +0100

    execute external program, openbrowser

--- http/server/server.go
+++ http/server/server.go
@@ -3,38 +3,18 @@ package server
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"net/http"
 	"net/url"
-	"os/exec"
-	"runtime"
 	"time"
+
+	"../../os/exec"
 )
-
-func openbrowser(url string) {
-	var err error
-
-	switch runtime.GOOS {
-	case "linux":
-		err = exec.Command("xdg-open", url).Start()
-	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		err = exec.Command("open", url).Start()
-	default:
-		err = fmt.Errorf("unsupported platform")
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
 
 func rootHandler(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("request url: ", request.RequestURI)
 	urlValues, _ := url.Parse(request.RequestURI)
-	fmt.Printf("request param1: %v", urlValues.Query().Get("param1"))
-	fmt.Printf("request param2: %v", urlValues.Query().Get("param2"))
+	fmt.Printf("request param1: %v\n", urlValues.Query().Get("param1"))
+	fmt.Printf("request param2: %v\n", urlValues.Query().Get("param2"))
 	fmt.Fprintf(response, "time: %v", time.Now().Format(time.RFC1123))
 }
 
@@ -60,8 +40,6 @@ func Example() {
 	time.Sleep(1 * time.Second)
 
 	fmt.Println("open browser")
-	openbrowser(fmt.Sprintf("http://127.0.0.1:%v/get?param1='hello'&param2=10", portNumber))
+	exec.Openbrowser(fmt.Sprintf("http://127.0.0.1:%v/get?param1='hello'&param2=10", portNumber))
 
-	fmt.Println("sleep for 10 seconds")
-	time.Sleep(10 * time.Second)
 }
