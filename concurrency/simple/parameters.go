036e6cf850464f4aa0c9e706f2b02d5d740315c2
cherkavi <technik7job@gmail.com>
2018-03-24 15:52:20 +0100

    goroutine closure vs parameter , thread parameter, thread copy of parameter

--- concurrency/simple/parameters.go
+++ concurrency/simple/parameters.go
@@ -15,6 +15,10 @@ func delayExecution(externalFunction func()) {
 	externalFunction()
 }
 
+type Data struct {
+	value string
+}
+
 func ExecuteWithParameter() {
 	fmt.Printf("\n ---- attempt to print message after change it ---- \n")
 	message := "first message"
@@ -33,5 +37,36 @@ func ExecuteWithParameter() {
 	message = "changed message"
 	fmt.Printf("message after start goroutine: %v \n", message)
 	time.Sleep(time.Second * 2)
+
+	fmt.Printf("\n ---- operate with closure data into go-routine ( using value from parent Thread) ----")
+	fmt.Printf("\n ---- object will have final value from Parent Thread ( 4 ) and address of object will be the same  ---- \n")
+	dataFromMainThread := Data{}
+	for i := 0; i < 5; i++ {
+		dataFromMainThread.value = fmt.Sprintf("closure data: %v", i)
+		go func() {
+			time.Sleep(time.Millisecond * 100)
+			for i := 0; i < 5; i++ {
+				fmt.Printf("%v  %#v  %p \n", dataFromMainThread, dataFromMainThread, &dataFromMainThread)
+				// time.Sleep(time.Millisecond * 50)
+			}
+		}()
+	}
+	time.Sleep(time.Second * 1)
+
+	fmt.Printf("\n ---- operate with copy of data into go-routine ( from parent Thread) ---- ")
+	fmt.Printf("\n ---- different values, different addresses of object ---- \n")
+	for i := 0; i < 5; i++ {
+		dataFromMainThread.value = fmt.Sprintf("copy of data: %v", i)
+		go func(dataFromMainThread Data) {
+			time.Sleep(time.Millisecond * 100)
+			for i := 0; i < 5; i++ {
+				fmt.Printf("%v  %#v  %p \n", dataFromMainThread, dataFromMainThread, &dataFromMainThread)
+				// time.Sleep(time.Millisecond * 50)
+			}
+		}(dataFromMainThread)
+	}
+	fmt.Println("    parent generator finished")
+	time.Sleep(time.Second * 1)
 	fmt.Println()
+
 }
