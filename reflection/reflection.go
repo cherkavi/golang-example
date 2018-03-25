70780d33c3bf5d43db3bceda2f3e3684dc0449cd
cherkavi <technik7job@gmail.com>
2018-03-25 17:08:03 +0200

    reflection

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -9,6 +9,25 @@ import (
 	"./struct_analyser"
 )
 
+func instanceof(v interface{}) { // input parameter - empty interface
+	typeOf := reflect.TypeOf(v)
+	if typeOf.Kind() == reflect.Int {
+		fmt.Println("integer type detected")
+	}
+	fmt.Printf("typeOf: [%#v] is [%v] \n", v, typeOf)
+
+	valueOf := reflect.ValueOf(v)
+	if valueOf.Kind() == reflect.Int {
+		fmt.Println("integer value detected: ", valueOf.Interface().(int)) // convert to real value and upcast
+	}
+	fmt.Printf("valueOf: [%#v] is [%v] by type [%v] is changable: [%v] \n", v, valueOf, valueOf.Type(), valueOf.CanSet())
+	// fmt.Println("Elements: ", valueOf.Elem())
+	// see patterns/creational/builder.go
+
+	fmt.Println()
+}
+
+// see https://blog.golang.org/laws-of-reflection
 func main() {
 	fmt.Println("\n ---------- example of copy data from one struct to another ----")
 	copy.ReflectCopy()
@@ -22,4 +41,12 @@ func main() {
 	fmt.Println("\n ---------- type of element  ----")
 	movie := copy.Movie{"Star Wars", 1979}
 	fmt.Printf("%#v    type is: %v \n", movie, reflect.TypeOf(movie))
+
+	fmt.Println("\n ---------- using any type of value to print -----")
+	instanceof(10)
+	instanceof("this is string")
+	s := "this is string pointer"
+	instanceof(&s)
+	instanceof(func() {})
+	instanceof(struct{}{}) // example of empty struct
 }
