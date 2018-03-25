4905061bc4cbcc1e851c76d06893fd6e0f139dcc
cherkavi <technik7job@gmail.com>
2018-03-25 09:11:28 +0200

    pattern generator

--- concurrency/channel/channel_generator.go
+++ concurrency/channel/channel_generator.go
@@ -2,6 +2,7 @@ package channel
 
 import (
 	"fmt"
+	"time"
 )
 
 func process() chan<- bool {
@@ -24,9 +25,28 @@ func process() chan<- bool {
 	return control
 }
 
+func integerGenerator(values ...int) <-chan int {
+	returnValue := make(chan int)
+	go func() {
+		var value int
+		for _, value = range values {
+			returnValue <- value
+			time.Sleep(time.Millisecond * 100)
+		}
+		close(returnValue)
+	}()
+	return returnValue
+}
+
 func ExampleOfChannelGenerator() {
 	fmt.Println("> execute process and receive channel as a control knob  ")
 	controlKnob := process()
 	controlKnob <- true
 	controlKnob <- false
+
+	fmt.Println("> print range of digits ")
+	for i := range integerGenerator(10, 11, 15, 8, 7, 5) {
+		fmt.Printf("%v  ", i)
+	}
+	fmt.Println()
 }
