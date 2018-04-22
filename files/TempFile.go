2b1e52e32dabee9715f4aaa7a3767d2af9005d6f
cherkavi <technik7job@gmail.com>
2018-04-22 23:03:20 +0200

    write data into temp file, write bytes into file, write byte array into file

--- files/TempFile.go
+++ files/TempFile.go
@@ -15,3 +15,16 @@ func createTempFile() (*os.File, error) {
     return os.OpenFile(tempFile.Name()+".jpg", os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
 }
 
+func writeAnswerIntoTempFile(values []byte) (*os.File, error) {
+    var tempFile *os.File
+    var err error
+    if tempFile, err = createTempFile(); err != nil {
+	log.Panic("can't create temp file ")
+	return nil, err
+    }
+    defer tempFile.Close()
+
+    tempFile.Write(values)
+    tempFile.Sync()
+    return tempFile, err
+}
